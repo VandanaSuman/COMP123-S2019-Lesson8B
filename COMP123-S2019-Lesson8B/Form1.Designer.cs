76a2e6ade925d1f24ffc2280a2e12f11e635d837
301045522 <301045522@PR-E216-17>
2019-06-26 15:27:32 -0400

    Added title label

--- COMP123-S2019-Lesson8B/Form1.Designer.cs
+++ COMP123-S2019-Lesson8B/Form1.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Titlelabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Titlelabel
+            // 
+            this.Titlelabel.AutoSize = true;
+            this.Titlelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.Titlelabel.Location = new System.Drawing.Point(39, 26);
+            this.Titlelabel.Name = "Titlelabel";
+            this.Titlelabel.Size = new System.Drawing.Size(107, 37);
+            this.Titlelabel.TabIndex = 0;
+            this.Titlelabel.Text = "Lab08";
             // 
             // Lab8Form
             // 
@@ -36,14 +47,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.Titlelabel);
             this.Name = "Lab8Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lab8";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Titlelabel;
     }
 }
 
